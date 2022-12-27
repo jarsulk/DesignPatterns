7267a15e851afdd60f0b539411db17d55102e42a
J S <jsjsjs000@gmail.com>
2022-12-27 09:56:49 +0100

    dev

---
+++ Creational/Monostate.cs
@@ -0,0 +1,10 @@
+﻿Monostate m1 = new Monostate();
+Monostate m2 = new Monostate();
+m1.Data = 1;
+Console.WriteLine(m2.Data); // 1
+Console.ReadLine();
+
+class Monostate {
+	private static int data = 0;
+	public int Data { get { return data; } set { data = value; } }
+}
